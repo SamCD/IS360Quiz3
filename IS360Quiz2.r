0842b44550fabeb1987cffdb4f9c8c684d5abb9a
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2014-09-12 21:24:23 -0400

    Create IS360Quiz2.r

---
+++ IS360Quiz2.r
@@ -0,0 +1,40 @@
+IS360Quiz3
+==========
+#1
+factorial(12)
+#2
+1500 * ((1 + (0.324 / 12)) ^ 72)
+#3
+vect <- (1:20)
+sum(vect[c(FALSE, FALSE, TRUE)])
+#4
+for(i in 1:10)
+{
+  x <- 2
+  x <- x + x ^ i
+}
+#5
+x <- 2
+while x <= 10
+#6
+z <- 1:10
+sum(2 ^ (z))
+#7
+t <- 20
+while (t <= 50)
+{
+  print(t)
+  t <- t + 5
+}
+#8
+charr <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+overandover <- rep("example", length(charr))
+print(overandover)
+#9
+#10
+vector <- 1:10
+mean(vector)
+#11
+mean(vector, na.rm=TRUE)
+#12
+#13
